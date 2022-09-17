4a13875614797421c0ad216df7490925ce1b5dc6
KrzysztofSnopek <108864848+KrzysztofSnopek@users.noreply.github.com>
2022-09-17 16:01:24 +0200

    Firebase before fixes

--- stock-app/src/Component/Signup.tsx
+++ stock-app/src/Component/Signup.tsx
@@ -1,10 +1,31 @@
-import { useRef } from 'react'
-import { Form, Button, Card } from 'react-bootstrap'
+import React, { useRef, useState } from 'react'
+import { Form, Button, Card, Alert } from 'react-bootstrap'
+import { useAuth } from '../Contexts/AuthContext'
 
 export function Signup() {
-  const emailRef = useRef(null)
-  const passwordRef = useRef(null)
-  const passwordConfirmRef = useRef(null)
+  const emailRef = useRef<HTMLInputElement>(null)
+  const passwordRef = useRef<HTMLInputElement>(null)
+  const passwordConfirmRef = useRef<HTMLInputElement>(null)
+  const { signup } = useAuth()
+  const [error, setError] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  async function handleSubmit(e: React.ChangeEvent<any>) {
+    e.preventDefault()
+
+    if (passwordRef?.current?.value !== passwordConfirmRef?.current?.value) {
+      return setError('Password is not correct')
+    }
+
+    try {
+      setError('')
+      setLoading(true)
+      await signup(emailRef?.current?.value, passwordRef?.current?.value) 
+    } catch {
+      setError('Failed to create an account')
+    }  
+    setLoading(false)
+  }
 
   return (
     <>
@@ -13,7 +34,8 @@ export function Signup() {
           <h2 className='text-center mb-4'>
             Sign Up
           </h2>
-          <Form>
+          {error && <Alert variant='danger'>{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
             <Form.Group id='email'>
               <Form.Label>Email</Form.Label>
               <Form.Control type='email' ref={emailRef} required />
@@ -29,7 +51,7 @@ export function Signup() {
               <Form.Control type='password' ref={passwordConfirmRef} required />
             </Form.Group>
 
-            <Button className='w-100' type='submit'>
+            <Button disabled={loading} className='w-100' type='submit'>
               Sign Up
             </Button>
           </Form>
