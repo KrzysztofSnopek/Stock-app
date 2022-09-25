6548265076dbc0949e7df8b6b2db324375618e12
KrzysztofSnopek <108864848+KrzysztofSnopek@users.noreply.github.com>
2022-09-25 17:41:23 +0200

    Displays columns of stock data from API

--- stock-app/src/Component/Dashboard.tsx
+++ stock-app/src/Component/Dashboard.tsx
@@ -57,11 +57,11 @@ export function Dashboard() {
     <div>
       <StockNavbar />
       <div className='mt-4 d-flex justify-content-center'>
-        <Card className='mt-4' style={{ minHeight: '75vh', maxWidth: '600px'}}>
+        <Card className='mt-4' style={{ maxWidth: '600px'}}>
           <Card.Body>
-            <h2 className='text-center mb-4'>
+            <div className='text-center mb-4'>
                 <Stock />
-            </h2>
+            </div>
             {error && <Alert variant='danger'>{error}</Alert>}
           </Card.Body>
         </Card>
--- stock-app/src/Component/Stock.tsx
+++ stock-app/src/Component/Stock.tsx
@@ -1,44 +1,95 @@
 import { useState, useEffect} from 'react'
-import axios from 'axios'
-
-
+import { Table, Button } from 'react-bootstrap'
 
 // var XMLParser = require('react-xml-parser');
-// var xml = new XMLParser().parseFromString(xmlText);    // Assume xmlText contains the example XML
+// var xml = new XMLParser().parseFromString(xmlText);
 // console.log(xml);
 // console.log(xml.getElementsByTagName('Name'));
 
-// const getAllStocks = () => {
-//     return axios.get(`http://webtask.future-processing.com:8068/stocks`)
-// }
+export interface ItemData {
+  name: string;
+  code: string;
+  unit: number;
+  price: number;
+}
 
-// interface StocksData
-
-
+interface StockData {
+  items: ItemData[];
+  publicationDate: string;
+}
 
 export function Stock(): JSX.Element {
 // useStaty
+const [singleStockData, setSingleStockData] = useState<StockData>()
 
 // useEffect z mapowaniem po elementach api
 useEffect(() => {
-	let interval = setInterval(async () => {
+    const fetchData = async () => {
 		const res = await fetch('http://webtask.future-processing.com:8068/stocks')
         .then((response) => response.text())
-        .then((xmlText) => console.log(xmlText))
+        .then((xmlText) => JSON.parse(xmlText))
+        .then((data) => data)
         .catch((error) => {
             console.log(error);
         });
-	}, 30000);
-	return () => {
-		clearInterval(interval);
-	};
+  setSingleStockData(res)}
+  fetchData();
+  setInterval(() => {
+    fetchData()
+  }, 30000) 
 }, []);
-
-
 
   return (
     <div>
-        <div>Stock</div> 
+      <h2>Stock prices</h2>
+      <Table size='sm'>
+        <thead>
+          <tr>
+            <th>Company</th>
+            <th>Code</th>
+            <th>Value</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {singleStockData?.items.map((stock, index) => {
+            return(
+              <tr key={index}>
+                <td>{stock.name}</td>
+                <td>{stock.code}</td>
+                <td>{stock.price}</td>
+                <td><Button>BUY</Button></td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </Table>
+
+      <h2>My wallet</h2>
+      <Table size='sm'>
+        <thead>  
+          <tr>
+            <th>Company</th>
+            <th>Code</th>
+            <th>Value</th>
+            <th>Number of units</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {singleStockData?.items.map((stock, index) => {
+            return(
+              <tr key={index}>
+                <td>{stock.name}</td>
+                <td>{stock.code}</td>
+                <td>{stock.price}</td>
+                <td>nr of bought</td>
+                <td><Button>SELL</Button></td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </Table>         
     </div>
     
   )
